33b9d1ca0e0508f2cfc1e53f728effe14a971c6e
Dhaval Chauhan (DC) <d.chauhan@hotmail.com>
2017-04-26 20:08:53 -0400

    Add files via upload

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(csc455.Startup))]
+namespace csc455
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
